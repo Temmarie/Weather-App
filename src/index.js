ac8497018b28ce3b465bc2f0cc447266d39a365b
Temmarie <temmar_whyte16@yahoo.com>
2020-10-12 14:09:43 +0100

    Fix temperature conversion

--- src/index.js
+++ src/index.js
@@ -1,57 +1,45 @@
-import './styles/index.css';
-
 const api = {
   key: '71a1d54910fc34b8989358aaf59ab093',
   base: 'https://api.openweathermap.org/data/2.5/',
 };
-
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);//eslint-disable-line
-
 const setQuery = (evt) => {
   if (evt.keyCode === 13) {
     getResults(searchbox.value);//eslint-disable-line
+    searchbox.value = '';//eslint-disable-line
   }
 };
-
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);//eslint-disable-line
 async function getResults(query) {
   await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => weather.json()).then(displayResults);//eslint-disable-line
 }
-
 const displayResults = (weather) => {
   const city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
-
   const now = new Date();
   const date = document.querySelector('.location .date');
   date.innerText = dateBuilder(now); //eslint-disable-line
 
-  // const temp = document.querySelector('.current .temp');
-  // temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-//code you sent
   const tempInCel = Math.round(weather.main.temp);
-  function tempInFar(temp) {return Math.round((temp * (9/5)) + 32);};
+  function tempInFar(temp) {
+    return Math.round((temp * (9/5)) + 32);//eslint-disable-line
+  }
   const newTemp = tempInFar(tempInCel);
-  const temp = document.querySelector('.current .temp'); 
-  temp.innerHTML = `${tempInCel}<span>°C</span>/ ${newTemp}<span>°F</span>`;
-//stops here
+  const temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${tempInCel}<span>°C</span> / ${newTemp}<span>°F</span>`;
+
   const weatherEl = document.querySelector('.current .weather');
   weatherEl.innerText = weather.weather[0].main;
-
   const highLow = document.querySelector('.hi-low');
   highLow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 };
-
 const dateBuilder = (d) => {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
   const day = days[d.getDay()];
   const date = d.getDate();
   const month = months[d.getMonth()];
   const year = d.getFullYear();
-
   return `${day} ${date} ${month} ${year}`;
 };
