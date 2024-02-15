4b06492d434ebf4f805a07e0560702b9474b66b1
Temmarie <temmar_whyte16@yahoo.com>
2024-02-15 12:52:53 +0100

    Add comments to previous code base

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//API configuration
 const api = {
   key: '71a1d54910fc34b8989358aaf59ab093',
   base: 'https://api.openweathermap.org/data/2.5/',
 };
+// Event listener for the search box
 const searchbox = document.querySelector('.search-box');
 const setQuery = (evt) => {
   if (evt.keyCode === 13) {
@@ -11,25 +13,33 @@ const setQuery = (evt) => {
   }
 };
 searchbox.addEventListener('keypress', setQuery);//eslint-disable-line
+
+// Function to Fetch Weather Data
 async function getResults(query) {
   await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => weather.json()).then(displayResults);//eslint-disable-line
 }
+//Function to Display Weather Data:
 const displayResults = (weather) => {
+  // Displaying city name and country code
   const city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  // Displaying current date
   const now = new Date();
   const date = document.querySelector('.location .date');
-  date.innerText = dateBuilder(now); //eslint-disable-line
+  date.innerText = dateBuilder(now);
 
-  // degree toggle
+  // Displaying temperature
   const tempInCel = Math.round(weather.main.temp);
+  // Function to convert temperature to Fahrenheit
   function tempInFar(temp) {
     return Math.round((temp * (9 / 5)) + 32);
   }
   const newTemp = tempInFar(tempInCel);
   const temp = document.querySelector('.current .temp');
   temp.innerHTML = `${tempInCel}°C`;
+  // Event listener to toggle temperature unit
   temp.addEventListener('click', () => {
     if (temp.innerHTML.includes('C')) {
       temp.innerHTML = `${newTemp}°F`;
@@ -38,11 +48,16 @@ const displayResults = (weather) => {
     }
   });
 
+  // Displaying weather description
   const weatherEl = document.querySelector('.current .weather');
   weatherEl.innerText = weather.weather[0].main;
+
+  // Displaying high and low temperatures
   const highLow = document.querySelector('.hi-low');
   highLow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 };
+
+//Function to Build Date String:
 const dateBuilder = (d) => {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
